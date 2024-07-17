8e4704ae0a5efa000cde84bf836830fcd820ecec
Parth <psomani16k@gmail.com>
2024-07-17 14:44:43 +0530

    initial commit

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cryptography_flutter
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CryptographyFlutterPlugin.register(with: registry.registrar(forPlugin: "CryptographyFlutterPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
---
+++ native/hub/src/lib.rs
@@ -0,0 +1,43 @@
+//! This `hub` crate is the
+//! entry point of the Rust logic.
+
+mod common;
+mod messages;
+use git2::Repository;
+use messages::basic::SmallText;
+use rinf::debug_print;
+use std::path::Path;
+
+use tokio;
+// Comment this line to target the web.
+// use tokio_with_wasm::alias as tokio;
+// Uncomment this line to target the web.
+
+rinf::write_interface!();
+
+// Use `tokio::spawn` to run concurrent tasks.
+// Always use non-blocking async functions
+// such as `tokio::fs::File::open`.
+// If you really need to use blocking code,
+// use `tokio::task::spawn_blocking`.
+async fn main() {
+    loop {
+        let mut rec = SmallText::get_dart_signal_receiver().unwrap();
+        let dir_path: String = "".to_string();
+        while let Some(dir_path) = rec.recv().await {
+            let dir_path = dir_path.message.text;
+            debug_print!("{dir_path}");
+
+            let dir_path = Path::new(&dir_path);
+            let url = "https://github.com/psomani16k/Diraudio";
+            let repo = match Repository::clone(url, dir_path) {
+                Ok(repo) => {
+                    debug_print!("{:?}", repo.path().to_str());
+                }
+                Err(e) => {
+                    debug_print!("failed to clone: {}", e);
+                }
+            };
+        }
+    }
+}
