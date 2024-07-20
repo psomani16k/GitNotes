cd97a32b9636d2851a9bd82982c41b8d3ea960b4
parth <psomani16k@gmail.com>
2024-07-20 10:32:01 +0530

    It compiled :`)

--- native/hub/src/lib.rs
+++ native/hub/src/lib.rs
@@ -4,7 +4,7 @@
 mod common;
 mod messages;
 use gix::{
-    clone::{self, fetch},
+
     refs::file::transaction::prepare,
 };
 // use gitoxide_core::repository;
@@ -35,27 +35,13 @@ async fn main() {
             let dir_path = Path::new(&dir_path);
 
             let url = "https://github.com/psomani16k/Diraudio";
-            // let repo = match Repository::clone(url, dir_path) {
-            //     Ok(repo) => {
-            //         debug_print!("{:?}", repo.path().to_str());
-            //     }
-            //     Err(e) => {
-            //         debug_print!("failed to clone: {}", e);
-            //     }
-            // };
-            let url = "https://github.com/de-vri-es/auth-git2-rs";
+            
 
             // Perform the clone operation
             let mut repo = gix::prepare_clone(url, dir_path).unwrap();
 
             // Optionally, perform additional operations with the cloned repository
-            let mut prepare_checkout = repo
-                .fetch_only(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)
-                .unwrap();
-            let prepare_checkout = prepare_checkout.0;
-            let mainRepo = prepare_checkout.main_repo().unwrap();
-            let workTree = mainRepo.work_dir().unwrap();
-            debug_print!("{:?}", workTree);
+           
         }
     }
 }
