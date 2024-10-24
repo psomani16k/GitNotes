7ce900bbfbf768c91feb43cd805904ec87195e61
parth <psomani16k@gmail.com>
2024-10-24 14:17:31 +0530

    using fixed app storage for repositories

--- lib/helpers/directory.dart
+++ lib/helpers/directory.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
-import 'package:file_picker/file_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DirectoryHelper {
   // singleton: private constructor and static instance
@@ -13,21 +11,7 @@ class DirectoryHelper {
   /// It creates a folder named GitNotes where everything will be stored.
   static Future<DirectoryHelper> getInstance() async {
     if (_instance == null) {
-      await Permission.manageExternalStorage.request();
-      await Permission.storage.request();
-
-      final SharedPreferences prefs = await SharedPreferences.getInstance();
-      String? homePath = prefs.getString("_homeDir");
-
-      if (homePath == null) {
-        homePath = await FilePicker.platform.getDirectoryPath();
-        if (homePath == null) {
-          return DirectoryHelper.getInstance();
-        }
-        homePath = "$homePath/GitNotes";
-        await prefs.setString("_homeDir", homePath);
-      }
-      Directory homeDir = await Directory(homePath).create(recursive: true);
+      Directory homeDir = (await getExternalStorageDirectory())!;
       _instance = DirectoryHelper._(homeDir);
     }
     return _instance!;
